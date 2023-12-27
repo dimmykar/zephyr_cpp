7bbe23b7eeb5234e58018f572bf5a1e042c7cd4e
Dmitry Karasev <karasevsdmitry@yandex.ru>
2023-12-27 08:29:40 +0300

    fw: implement gpio driver

---
+++ firmware/include/gpio.hpp
@@ -0,0 +1,233 @@
+/**
+ * @file           : gpio.hpp
+ * @author         : Dmitry Karasev <karasevsdmitry@yandex.ru>
+ * @brief          : MCU General Purpose IO (GPIO) peripheral driver
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Dmitry Karasev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <memory>
+#include <zephyr/drivers/gpio.h>
+
+using device_t = struct device;
+using gpio_callback_t = struct gpio_callback;
+using gpio_irq_handler_fn = void (*)(void *);
+
+namespace driver
+{
+
+namespace gpio
+{
+
+/**
+ * @brief           Possible GPIO Pin hardware states
+ */
+enum class pin_state_t
+{
+    Reset,                                  /*!< GPIO Pin is in LOW state */
+    Set                                     /*!< GPIO Pin is in HIGH state */
+};
+
+/**
+ * @brief           Possible GPIO Pin logical level states
+ * @details         Logical state takes into account \ref gpio_t::is_active_low flag
+ */
+enum class pin_active_state_t
+{
+    Inactive,                               /*!< GPIO Pin is in Logical 0 state */
+    Active                                  /*!< GPIO Pin is in Logical 1 state */
+};
+
+/**
+ * @brief           Possible GPIO Pin Output Mode configurations
+ */
+enum class pin_output_mode_t
+{
+    PushPull,                               /*!< GPIO Pin operates in "Push-Pull" mode */
+    OpenDrain                               /*!< GPIO Pin operates in "Open Drain" mode */
+};
+
+/**
+ * @brief           Possible GPIO Pin Slew Rate configurations
+ * @details         Determines how quickly the pin switches to a new state
+ */
+enum class pin_output_slew_t
+{
+    Slow,                                   /*!< GPIO Pin switches slowly (good for solid LEDs, buttons, etc.) */
+    Medium,                                 /*!< GPIO Pin switches with medium speed (good for slow peripherals) */
+    Fast,                                   /*!< GPIO Pin switches fast (good for fast peripherals) */
+    VeryFast                                /*!< GPIO Pin switches very fast (good for high-speed peripherals) */
+};
+
+/**
+ * @brief           Possible GPIO Pin Input Bias configurations
+ */
+enum class pin_pull_t
+{
+    Float,                                  /*!< GPIO Pin is in floating input state */
+    PullUp,                                 /*!< GPIO Pin is pulled to HIGH level */
+    PullDown                                /*!< GPIO Pin is pulled to LOW level */
+};
+
+/**
+ * @brief           Possible GPIO Pin interrupt trigger configurations
+ */
+enum class pin_irq_trigger_t
+{
+    EdgeToActive,                           /*!< Detect Edge to Active state (Rising Edge in case of Active HIGH) */
+    EdgeToInactive,                         /*!< Detect Edge to Inactive state (Rising Edge in case of Active LOW) */
+    EdgeAny                                 /*!< Detect any switches to both states */
+};
+
+/**
+ * @brief           Handle for attached to GPIO Pin IRQ Handler Callback
+ */
+struct gpio_irq_wrapper_t
+{
+    gpio_callback_t cb_ctx;                 /*!< IRQ Handler Callback context */
+    gpio_irq_handler_fn irq_handler;        /*!< Pointer to attached IRQ Handler callback */
+    void *arg;                              /*!< Argument for attached IRQ Handler callback */
+};
+
+/**
+ * @brief           GPIO Pin driver class
+ * @details         Controls specified GPIO Pin operation in Digital Input
+ *                      or Digital Output mode
+ * @todo            Implement Analog Input mode APIs
+ */
+class gpio_t
+{
+public:
+    /**
+     * @brief          Constructor
+     * @param[in]      port_ptr Pointer to GPIO Port device handle
+     * @param[in]      pin GPIO Pin number in specified GPIO Port
+     * @param[in]      is_active_low `true` if GPIO Pin Active state is LOW,
+     *                     so as not to care about the inverted logic
+     */
+    gpio_t(const device_t *port_ptr, uint8_t pin, bool is_active_low = false);
+
+    /**
+     * @brief          Configure GPIO Pin as Output
+     * @param[in]      omode GPIO Pin Output mode
+     * @param[in]      init_state GPIO Pin initial state after configuring
+     * @param[in]      speed GPIO Pin slew rate
+     * @return         `true` if GPIO Pin configured successfully,
+     *                     `false` if
+     *                         - GPIO Port is not exists
+     *                         - GPIO Pin configuration failed
+     */
+    bool config_as_output(pin_output_mode_t omode, pin_active_state_t init_state = pin_active_state_t::Inactive,
+                              pin_output_slew_t speed = pin_output_slew_t::Slow);
+
+    /**
+     * @brief          Set GPIO Pin to configured Active State
+     */
+    void set();
+
+    /**
+     * @brief          Reset GPIO Pin to configured Inactive State
+     */
+    void reset();
+
+    /**
+     * @brief          Toggle current state of GPIO Pin
+     */
+    void toggle();
+
+    /**
+     * @brief          Configure GPIO Pin as Input
+     * @param[in]      pull GPIO Pin bias pull
+     * @return         `true` if GPIO Pin configured successfully,
+     *                     `false` if
+     *                         - GPIO Port is not exists
+     *                         - GPIO Pin configuration failed
+     */
+    bool config_as_input(pin_pull_t pull);
+
+    /**
+     * @brief          Read current physical state of GPIO Pin
+     * @return         Member of \ref pin_state_t
+     */
+    pin_state_t read_state();
+
+    /**
+     * @brief          Read current configured Active state of GPIO Pin
+     * @return         Member of \ref pin_active_state_t
+     */
+    pin_active_state_t read_active_state();
+
+    /**
+     * @brief          Enable Interrupt for GPIO Pin and attach IRQ Handler callback for it
+     * @param[in]      irq_handler Pointer to IRQ Handler callback
+     * @param[in]      irq_handler_arg Argument for attached IRQ Handler callback
+     * @param[in]      irq_trigger GPIO Pin interrupt trigger source
+     * @return         `true` if interrupt configured successfully,
+     *                     `false` if
+     *                         - Passed nullptr IRQ Handler callback
+     *                         - GPIO Pin is not configured as Input
+     *                         - Failed to attach IRQ Handler callback
+     */
+    bool attach_irq(gpio_irq_handler_fn irq_handler, void *irq_handler_arg, pin_irq_trigger_t irq_trigger);
+
+    /**
+     * @brief          Disable Interrupt for GPIO Pin and detach its IRQ Handler callback
+     * @return         `true` if IRQ Handler callback detached successfully, `false` otherwise
+     */
+    bool detach_irq();
+
+private:
+    /**
+     * @brief          Common IRQ Handler callback
+     * @details        Callback with signature required by Zephyr GPIO API.
+     *                     Delegates IRQ Handling to configured IRQ Handler callback
+     * @param[in]      port Pointer to GPIO Port device handle
+     * @param[in]      cb Pointer to IRQ Handler Callback context
+     * @param[in]      pins A bit mask of relevant pins for the IRQ Handler callback
+     */
+    static void pin_irq_handler(const device_t *port, gpio_callback_t *cb, gpio_port_pins_t pins);
+
+    /**
+     * @brief          Pointer to controlling GPIO Port device handle
+     */
+    const device_t *port_ptr;
+
+    /**
+     * @brief          Controlling GPIO Pin of the specified GPIO Port
+     */
+    uint8_t pin;
+
+    /**
+     * @brief          `true` if GPIO Pin Active state is LOW,
+     *                     `false` if GPIO Pin Active state is HIGH
+     */
+    bool is_active_low;
+
+    /**
+     * @brief          Pointer to IRQ Handler callback wrapper handle
+     */
+    std::unique_ptr<gpio_irq_wrapper_t> irq_ctx;
+};
+
+} // gpio
+
+} // driver
---
+++ firmware/source/gpio.cpp
@@ -0,0 +1,167 @@
+/**
+ * @file           : gpio.cpp
+ * @author         : Dmitry Karasev <karasevsdmitry@yandex.ru>
+ * @brief          : MCU General Purpose IO (GPIO) peripheral driver
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Dmitry Karasev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
+
+#include "gpio.hpp"
+
+#include <zephyr/kernel.h>
+
+using namespace driver::gpio;
+
+gpio_t::gpio_t(const device_t *port_ptr, uint8_t pin, bool is_active_low)
+    : port_ptr(port_ptr), pin(pin), is_active_low(is_active_low)
+{
+    // Raise abort() if std::bad_alloc exception throws with -fno-exceptions
+    this->irq_ctx = std::make_unique<gpio_irq_wrapper_t>();
+}
+
+bool gpio_t::config_as_output(pin_output_mode_t omode, pin_active_state_t init_state, pin_output_slew_t speed)
+{
+    if (!device_is_ready(this->port_ptr)) {
+        return false;
+    }
+
+    gpio_flags_t output_flags = (init_state == pin_active_state_t::Inactive) ? GPIO_OUTPUT_INACTIVE : GPIO_OUTPUT_ACTIVE;
+    if (omode == pin_output_mode_t::OpenDrain) {
+        output_flags |= GPIO_OPEN_DRAIN;
+    }
+    if (this->is_active_low) {
+        output_flags |= GPIO_ACTIVE_LOW;
+    }
+
+    // TODO Configure slew rate
+    ARG_UNUSED(speed);
+
+    int32_t ret = gpio_pin_configure(this->port_ptr, this->pin, output_flags);
+    if (ret < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+void gpio_t::set()
+{
+    gpio_pin_set(this->port_ptr, this->pin, 1);
+}
+
+void gpio_t::reset()
+{
+    gpio_pin_set(this->port_ptr, this->pin, 0);
+}
+
+void gpio_t::toggle()
+{
+    gpio_pin_toggle(this->port_ptr, this->pin);
+}
+
+bool gpio_t::config_as_input(pin_pull_t pull)
+{
+    if (!device_is_ready(this->port_ptr)) {
+        return false;
+    }
+
+    gpio_flags_t input_flags = GPIO_INPUT;
+    if (pull == pin_pull_t::PullUp) {
+        input_flags |= GPIO_PULL_UP;
+    }
+    else if (pull == pin_pull_t::PullDown) {
+        input_flags |= GPIO_PULL_DOWN;
+    }
+    if (this->is_active_low) {
+        input_flags |= GPIO_ACTIVE_LOW;
+    }
+
+    int32_t ret = gpio_pin_configure(this->port_ptr, this->pin, input_flags);
+    if (ret < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+pin_state_t gpio_t::read_state()
+{
+    return (gpio_pin_get_raw(this->port_ptr, this->pin) == 1) ? pin_state_t::Set : pin_state_t::Reset;
+}
+
+pin_active_state_t gpio_t::read_active_state()
+{
+    return (gpio_pin_get(this->port_ptr, this->pin) == 1) ? pin_active_state_t::Active : pin_active_state_t::Inactive;
+}
+
+bool gpio_t::attach_irq(gpio_irq_handler_fn irq_handler, void *irq_handler_arg, pin_irq_trigger_t irq_trigger)
+{
+    if (irq_handler == nullptr) {
+        return false;
+    }
+
+    if (!(gpio_pin_is_input(this->port_ptr, this->pin) == 1)) {
+        return false;
+    }
+
+    gpio_flags_t edge_flags = 0;
+    if (irq_trigger == pin_irq_trigger_t::EdgeToActive) {
+        edge_flags |= GPIO_INT_EDGE_TO_ACTIVE;
+    }
+    else if (irq_trigger == pin_irq_trigger_t::EdgeToInactive) {
+        edge_flags |= GPIO_INT_EDGE_TO_INACTIVE;
+    }
+    else if (irq_trigger == pin_irq_trigger_t::EdgeAny) {
+        edge_flags |= GPIO_INT_EDGE_BOTH;
+    }
+    int32_t ret = gpio_pin_interrupt_configure(this->port_ptr, this->pin, edge_flags);
+    if (ret < 0) {
+        return false;
+    }
+
+    this->irq_ctx->irq_handler = irq_handler;
+    this->irq_ctx->arg = irq_handler_arg;
+    gpio_init_callback(&this->irq_ctx->cb_ctx, gpio_t::pin_irq_handler, BIT(this->pin));
+    ret = gpio_add_callback(this->port_ptr, &this->irq_ctx->cb_ctx);
+    if (ret < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool gpio_t::detach_irq()
+{
+    if (!(gpio_pin_is_input(this->port_ptr, this->pin) == 1)) {
+        return false;
+    }
+
+    int32_t ret = gpio_remove_callback(this->port_ptr, &this->irq_ctx->cb_ctx);
+    if (ret < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+void gpio_t::pin_irq_handler(const device_t *port, gpio_callback_t *cb, gpio_port_pins_t pins)
+{
+    struct gpio_irq_wrapper_t *container = CONTAINER_OF(cb, gpio_irq_wrapper_t, cb_ctx);
+    container->irq_handler(container->arg);
+}
