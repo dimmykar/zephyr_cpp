c54e84380faf06cda3a80fff5c10cb2b2d573619
Dmitry Karasev <karasevsdmitry@yandex.ru>
2024-03-26 21:48:24 +0300

    fw: main: implement leds control demo

--- firmware/source/main.cpp
+++ firmware/source/main.cpp
@@ -27,8 +27,12 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
 #include "leds_controller.hpp"
+#include "button.hpp"
+
+using namespace driver;
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -40,15 +44,25 @@ int main(void)
 {
     LOG_INF("Hello from Zephyr RTOS");
 
+    struct gpio_dt_spec user_button_dt = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
+    driver::button_t user_btn{user_button_dt.port, user_button_dt.pin};
+    user_btn.init(driver::gpio::pin_pull_t::Float, driver::gpio::pin_irq_trigger_t::EdgeToActive);
+
     leds_controller_t &leds_ctrl = leds_controller_t::get_instance();
     if (!leds_ctrl.init()) {
         LOG_ERR("Failed to initialize leds controller");
         return 0;
     }
 
+    bool is_silent = false;
     for (;;)
     {
-        k_msleep(500U);
+        if (user_btn.is_pressed()) {
+            is_silent ? leds_ctrl.enable_silent_mode() : leds_ctrl.disable_silent_mode();
+            is_silent = !is_silent;
+        }
+
+        k_msleep(100U);
     }
 
     return 0;
